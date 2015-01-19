38f87a1cd79f29c05fc5dbd05ae6954e00925137
xlgwr <xlgwr@163.com>
2015-01-19 16:52:17 +0800

    init first

---
+++ rdlc.Matrix/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ReportsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.NorthwindDataSet = new ReportsApplication3.NorthwindDataSet();
+            this.vReportProductsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.vReportProductsTableAdapter = new ReportsApplication3.NorthwindDataSetTableAdapters.vReportProductsTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.NorthwindDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vReportProductsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.vReportProductsBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "ReportsApplication3.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(682, 356);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // NorthwindDataSet
+            // 
+            this.NorthwindDataSet.DataSetName = "NorthwindDataSet";
+            this.NorthwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // vReportProductsBindingSource
+            // 
+            this.vReportProductsBindingSource.DataMember = "vReportProducts";
+            this.vReportProductsBindingSource.DataSource = this.NorthwindDataSet;
+            // 
+            // vReportProductsTableAdapter
+            // 
+            this.vReportProductsTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 356);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NorthwindDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vReportProductsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource vReportProductsBindingSource;
+        private NorthwindDataSet NorthwindDataSet;
+        private NorthwindDataSetTableAdapters.vReportProductsTableAdapter vReportProductsTableAdapter;
+    }
+}
+
---
+++ rdlc.Matrix/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReportsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'NorthwindDataSet.vReportProducts' table. You can move, or remove it, as needed.
+            this.vReportProductsTableAdapter.Fill(this.NorthwindDataSet.vReportProducts);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
